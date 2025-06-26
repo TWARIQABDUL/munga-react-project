5b11dd49ef5521c6895389c1fdf8383c66180c0f
TWARIQABDUL <tariqazizibabu@gmail.com>
2025-06-26 15:35:02 +0300

    Implement mobile navigation menu and update styles for mobile view

--- src/components/NavContainer.jsx
+++ src/components/NavContainer.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 import menubtn from '../assets/icon-menu.svg'
+import MobileList from '../utils/MobileList'
 function NavContainer() {
-  return (
-     <header id="header">
-        <nav>
-            <h1>traver</h1>
-            <ul>
-                <li><a href="#services">Service</a></li>
-                <li><a href="#department">Department</a></li>
-                <li><a href="#news-lette">Newsleter</a></li>
-                <li><a href="#footer">Site Map</a></li>
-            </ul>
-            <div className="buttons_auth">
-                <button id="register">Signup</button>
-                <button className="mobile-menu" id="menu"> <img src={menubtn} alt=""/></button>
-            </div>
-        </nav>
-    </header>
-  )
+    const [isVisible, setNavVisible] = useState(false)
+    const tagleMenu = ()=>{
+        console.log("here");
+        
+        setNavVisible(!isVisible)
+    }
+    return (
+        <>
+            <header id="header">
+                <nav>
+                    <h1>traver</h1>
+                    <ul>
+                        <li><a href="#services">Service</a></li>
+                        <li><a href="#department">Department</a></li>
+                        <li><a href="#news-lette">Newsleter</a></li>
+                        <li><a href="#footer">Site Map</a></li>
+                    </ul>
+                    <div className="buttons_auth">
+                        <button id="register">Signup</button>
+                        <button className="mobile-menu" id="menu" onClick={() => tagleMenu()}> <img src={menubtn} alt="" /></button>
+                    </div>
+
+                </nav>
+            </header>
+
+            {isVisible && <MobileList setStatus={tagleMenu} />}
+
+        </>
+    )
 }
 
 export default NavContainer
---
+++ src/utils/MobileList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import closee from '../assets/icon-close.svg'
+function MobileList({setStatus}) {
+    console.log(setStatus);
+    
+  return (
+    <>
+     <div className="mobile-menu-container ">
+        <button className="close-menu" id="close" onClick={setStatus} ><img src={closee} alt=""/></button>
+        <ul>
+            <li><a href="/about.html">Home</a></li>
+            <li><a href="/about.html">About Us</a></li>
+            <li><a href="contactus.html">Contact Us</a></li>
+        </ul>
+    </div>
+    </>
+  )
+}
+
+export default MobileList
