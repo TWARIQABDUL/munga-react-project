5572beb264c5d69e6020bd7ba47d9ccc1af98cb0
TWARIQABDUL <tariqazizibabu@gmail.com>
2025-06-26 12:26:49 +0300

    [Initial Commit] Clean the template

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import NavContainer from './components/NavContainer'
+import HeroSection from './components/HeroSection'
+import Services from './components/Services'
+import Subscribe from './components/Subscribe'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <NavContainer />
+      <HeroSection />
+      <Services />
+      <Subscribe />
+      <Footer />
+    </>
+  )
+}
+
+export default App
+
+
+
