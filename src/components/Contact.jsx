a26b6ba6829f6972e2e79bcb2ed06521cc17f86e
TWARIQABDUL <tariqazizibabu@gmail.com>
2025-07-02 16:42:20 +0300

    Refactor Contact component to use InputGroup for form fields; update MobileList links for consistency and remove console.log

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import InputGroup from './InputGroup'
 
 function Contact() {
   return (
@@ -6,17 +7,10 @@ function Contact() {
             <div class="form-container">
                 <h2>Contact Us</h2>
                 <form action="#" method="post">
+                    <InputGroup label="Name" type="text" />
+                    <InputGroup label="Email" type="email" />
                     <div class="form-group">
-                        <label for="name">Name:</label>
-                        <input type="text" id="name" name="name" required=""/>
-                        
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" name="email" required=""/>
-                    </div>
-                    <div class="form-group">
-                        <label for="message">Message</label>
+                        <label forHtml="message">Message</label>
                         <textarea id="message" name="message" rows="4" required=""></textarea>
                     </div>
                     <button>Send</button>
--- src/components/MobileList.jsx
+++ src/components/MobileList.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 
 import closee from '/assets/icon-close.svg'
+import { Link } from 'react-router-dom';
 function MobileList({setStatus}) {
-    console.log(setStatus);
-    
   return (
     <>
      <div className="mobile-menu-container ">
         <button className="close-menu" id="close" onClick={setStatus} ><img src={closee} alt=""/></button>
         <ul>
-            <li><Link to="/about.html">Home</Link></li>
-            <li><Link to="/about.html">About Us</Link></li>
-            <li><Link to="contactus.html">Contact Us</Link></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
         </ul>
     </div>
     </>
